a77fad7b59bbf1d8ba7c9c3298e0ad2e8b33df4b
Bakops <65463838+Bakops@users.noreply.github.com>
2024-05-26 13:11:48 +0200

    Linkme

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const btns = document.querySelectorAll(".btn-first");
 const search = document.querySelectorAll(".input_search");
 const menu = document.querySelectorAll(".menu");
 const image = document.querySelectorAll(".img_illus");
+const media = document.querySelectorAll(".media");
 
 window.addEventListener("load", () => {
   const TL = gsap.timeline({ paused: true });
@@ -18,6 +19,7 @@ window.addEventListener("load", () => {
     .staggerFrom(btns, 1, { opacity: 0, ease: "power2.out" }, 0.3, "-=1")
     .from(logo, 0.4, { transform: "scale(0)", ease: "power2.out" }, "-=2")
     .from(search, 0.4, { transform: "scale(0)", ease: "power2.out" }, "-=2")
+    .staggerFrom(media, 1, { right: -200, ease: "power2.out" }, 0.3, "-=1")
     .from(image, 0.4, { transform: "scale(0)", ease: "power2.out" }, "-=2");
 
   TL.play();
